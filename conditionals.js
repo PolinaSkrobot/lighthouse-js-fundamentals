bf5ab269c52e451e93433753d4cb91f132e0380b
Polina Skrobot <chi.polly.no@gmail.com>
2021-07-18 04:18:52 +0000

    Add more conditional examlpes

--- conditionals.js
+++ conditionals.js
@@ -1,10 +1,15 @@
 const raining = false;
 
-const temperature = 16;
+const temperature = 30;
 if (raining) {
   console.log("Don't forget your umbrella!");
 }
-
+if (!raining) {
+  console.log("Leave your umbrella at home!");
+}
+if (temperature < -40 || temperature > 40) {// cheking extreme weather
+  console.log("Maybe going outside isn't such a great idea…");
+} else {//checking appropriate weather
 if (temperature < 0) {
   console.log("Make sure you pick out a scarf!");
 } else if (temperature < 15) {
@@ -13,3 +18,4 @@ if (temperature < 0) {
   console.log("Short sleeves are fine.");
 }
 console.log("Now you're ready to go outside!");
+}
