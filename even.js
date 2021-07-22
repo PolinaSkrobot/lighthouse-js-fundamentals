30f08c9801ac76b84f1001531da08db690fecd5c
Polina Skrobot <chi.polly.no@gmail.com>
2021-07-22 00:58:40 +0000

    Even?true/false

---
+++ even.js
@@ -0,0 +1,9 @@
+const isEven = function (num) {
+  return num%2===0;
+}
+/*const tenEven = isEven(10);
+const elevenEven = isEven(11);
+console.log(tenIsEven);
+console.log(elevenIsEven);*/
+console.log(isEven(10));//reduction of commented code above
+console.log(isEven(11));
